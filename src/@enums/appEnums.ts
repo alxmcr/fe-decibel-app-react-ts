76299546bab611a472894ecece1656aedd40c15c
Alejandro Coca <alxmcr@outlook.com>
2024-04-26 14:47:43 -0400

    initial commit

---
+++ src/@enums/appEnums.ts
@@ -0,0 +1,12 @@
+export enum PlayerStatus {
+  IDLE = '',
+  NOW_PLAYING = 'Now Playing',
+  PAUSED = 'Paused',
+}
+
+export enum LoadingStates {
+  IDLE = 'IDLE',
+  PENDING = 'PENDING',
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR',
+}
