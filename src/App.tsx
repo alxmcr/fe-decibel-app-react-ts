c21af81f7f62b908075a0074be5aa4799c75aa34
Alejandro Coca <alxmcr@outlook.com>
2024-04-26 14:36:07 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
 export default function App() {
-  const x = 123;
-  console.log('🚀 ~ App ~ x:', x);
-  const myArray: string[] = ['hello', 'world'];
-  const element1 = myArray[2];
-  console.log(element1.length);
-  console.log('🚀 ~ App ~ element1:', element1);
-
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
