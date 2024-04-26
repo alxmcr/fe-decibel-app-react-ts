0988c003094be0092c7f2ee4315f71b1fd6f414d
Alejandro Coca <alxmcr@outlook.com>
2024-04-26 18:24:10 -0400

    wip

--- src/@services/PlaylistService/index.ts
+++ src/@services/PlaylistService/index.ts
@@ -1,5 +1,5 @@
 import { MOCK_PLAYLIST_001, MOCK_PLAYLIST_002, MOCK_PLAYLIST_003 } from '../../@mocks/mock-playlists';
-import { PlaylistData } from '../../@types/serviceTypes';
+import { PlaylistData, SongInPlaylistData } from '../../@types/serviceTypes';
 import { IPlaylistService } from './IPlaylistService';
 
 export class PlaylistService implements IPlaylistService {
@@ -33,6 +33,32 @@ export class PlaylistService implements IPlaylistService {
         // Simulate a successful response
         resolve(playlistResponse);
       }, 1000); // Simulate a delay of 1 second
+    });
+  }
+
+  findSongOnPlaylistById(playlist: PlaylistData, songId = ''): Promise<SongInPlaylistData> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (playlist === null) {
+          reject('Playlist is not valid');
+        }
+
+        if (playlist.songs === null) {
+          reject('Playlist does not have songs.');
+        }
+
+        if (playlist.songs.length === 0) {
+          reject('Playlist is empty.');
+        }
+
+        const song = playlist.songs.find((songInPlaylist) => songInPlaylist.id === songId);
+
+        if (song === null || song === undefined) {
+          reject(`There is not a song with this id ${songId}`);
+        } else {
+          resolve(song);
+        }
+      }, 1000);
     });
   }
 }
