6de83c028e0c42b1ae8b98e166de59d36e9e586e
Alejandro Coca <alxmcr@outlook.com>
2024-04-27 01:24:21 -0400

    wip

--- src/components/@playlist-dialog/HeaderDialogPlaylist.tsx
+++ src/components/@playlist-dialog/HeaderDialogPlaylist.tsx
@@ -4,11 +4,11 @@ export default function HeaderDialogPlaylist() {
   const closeDialogPlaylist = () => {};
 
   return (
-    <header className="relative flex h-[110px] w-full items-center justify-center border bg-perano-300 text-perano-600">
-      <button className="absolute left-5 md:hover:text-light-50" onClick={closeDialogPlaylist}>
+    <header className="relative flex h-[110px] w-full items-center justify-center rounded-lg border bg-perano-500 text-light-50">
+      <button className="absolute left-5 md:hover:text-perano-200" onClick={closeDialogPlaylist}>
         <Icon30x30ArrowBack />
       </button>
-      <h3 className="text-center text-[1.35rem] font-semibold text-perano-600">Playlist</h3>
+      <h3 className="text-center text-[1.35rem] font-semibold">Playlist</h3>
     </header>
   );
 }
