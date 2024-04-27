d10940a47b82fd426bdbc7338fead8773b878760
Alejandro Coca <alxmcr@outlook.com>
2024-04-27 14:49:47 -0400

    wip

--- src/store/@actions-creators/playlistActions.ts
+++ src/store/@actions-creators/playlistActions.ts
@@ -1,6 +1,33 @@
 import { PlaylistAction } from '../../@types/playlistReducerTypes';
 import { PlaylistData } from '../../@types/serviceTypes';
 
+export const initFetchingPlaylist = (dispatch: React.Dispatch<PlaylistAction>) => {
+  dispatch({
+    type: 'init-fetching-playlist',
+  });
+};
+
+export const successFetchingPlaylist = (
+  dispatch: React.Dispatch<PlaylistAction>,
+  playlistResponse: PlaylistData,
+) => {
+  dispatch({
+    type: 'success-fetching-playlist',
+    payload: {
+      playlistResponse,
+    },
+  });
+};
+
+export const errorFetchingPlaylist = (dispatch: React.Dispatch<PlaylistAction>, errorResponse: Error) => {
+  dispatch({
+    type: 'error-fetching-playlist',
+    payload: {
+      errorResponse,
+    },
+  });
+};
+
 export const selectPlaylistToPlay = (
   dispatch: React.Dispatch<PlaylistAction>,
   currentPlaylist: PlaylistData,
