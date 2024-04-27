a204d3a5ab5e0ace944a8a9cefdfdd7231b0510d
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-04-27 13:15:15 -0400

    Chore/improvements select song (#6)

--- src/components/@playlist-dialog/PlaylistSongListItem.tsx
+++ src/components/@playlist-dialog/PlaylistSongListItem.tsx
@@ -3,36 +3,42 @@ import { songPlaylist001 } from '../../@mocks/mock-songs-in-playlist';
 import { SongInPlaylistData } from '../../@types/serviceTypes';
 
 type Props = {
-  song: SongInPlaylistData;
+  songInPlaylist: SongInPlaylistData;
 };
 
-export default function PlaylistSongListItem({ song }: Props) {
-  const [isSongPlaying, setIsSongPlaying] = React.useState(false);
+export default function PlaylistSongListItem({ songInPlaylist }: Props) {
+  const [isSelectedToPlay, setIsSelectedToPlay] = React.useState(false);
 
-  const songIsPlaying = songPlaylist001;
-  const selectSongToPlay = () => {};
+  const currentSongPlaying = songPlaylist001;
+  const selectSongToPlay = () => {
+    console.log('Song selected', songInPlaylist);
+  };
 
   React.useEffect(() => {
-    setIsSongPlaying(songIsPlaying?.id === song.id);
-  }, [song.id, songIsPlaying?.id]);
+    setIsSelectedToPlay(currentSongPlaying?.id === songInPlaylist.id);
+  }, [songInPlaylist.id, currentSongPlaying?.id]);
 
-  if (song === null || song === undefined) {
+  if (songInPlaylist === null || songInPlaylist === undefined) {
     return null;
   }
 
   return (
     <li
-      className={`${isSongPlaying ? 'is-song-playing' : 'bg-white text-perano-300'} box-border flex max-h-[160px] items-center justify-between gap-3 rounded-lg border p-3 hover:bg-perano-300 hover:text-white`}
+      className={`${isSelectedToPlay ? 'is-selected-to-play' : 'bg-white text-perano-300'} box-border flex max-h-[160px] items-center justify-between gap-3 rounded-lg border p-3 hover:bg-perano-300 hover:text-white`}
       onClick={selectSongToPlay}
     >
       <div className="flex items-center gap-2">
-        <img src={song.albumCoverUrl} alt={song.name} className="size-[50px] rounded-full shadow-lg" />
+        <img
+          src={songInPlaylist.albumCoverUrl}
+          alt={songInPlaylist.name}
+          className="size-[50px] rounded-full shadow-lg"
+        />
         <div className="flex flex-col gap-1">
-          <h2 className="text-[1.2rem] font-bold text-black">{song.name}</h2>
-          <p>{song.singer}</p>
+          <h2 className="text-[1.2rem] font-bold text-black">{songInPlaylist.name}</h2>
+          <p>{songInPlaylist.singer}</p>
         </div>
       </div>
-      <p>{song.duration}</p>
+      <p>{songInPlaylist.duration}</p>
     </li>
   );
 }
