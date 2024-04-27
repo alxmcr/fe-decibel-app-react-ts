15d73179a663e9b8c126eb657cd287dfa8255449
Alejandro Coca <alxmcr@outlook.com>
2024-04-26 23:45:38 -0400

    wip

--- src/components/@player/PlayerProgressBar.tsx
+++ src/components/@player/PlayerProgressBar.tsx
@@ -1,9 +1,11 @@
 import PlayerProgressBarElapsedTime from './PlayerProgressBarElapsedTime';
 
 export default function ProgressBar() {
+  const percentageElapsedTime = 50;
+
   return (
-    <div>
-      <PlayerProgressBarElapsedTime />
+    <div className="relative h-[8px] w-full rounded-xl bg-perano-200">
+      {percentageElapsedTime > 0 ? <PlayerProgressBarElapsedTime /> : null}
     </div>
   );
 }
