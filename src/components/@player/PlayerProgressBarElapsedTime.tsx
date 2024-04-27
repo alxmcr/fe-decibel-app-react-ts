f73920dee0dfd7174fa3a17a4cb175fe69f5633e
Alejandro Coca <alxmcr@outlook.com>
2024-04-26 23:48:26 -0400

    wip

--- src/components/@player/PlayerProgressBarElapsedTime.tsx
+++ src/components/@player/PlayerProgressBarElapsedTime.tsx
@@ -3,6 +3,10 @@ type Props = {
 };
 
 export default function PlayerProgressBarElapsedTime({ percentageElapsedTime = 0 }: Props) {
+  if (percentageElapsedTime === 0) {
+    return null;
+  }
+
   return (
     <div
       className={`animation-player-progress-bar absolute h-[8px] rounded-xl bg-perano-600`}
