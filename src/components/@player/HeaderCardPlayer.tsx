51d4ef51786f62dde14d6de66878b64a4d904ef4
Alejandro Coca <alxmcr@outlook.com>
2024-04-26 23:13:55 -0400

    wip

---
+++ src/components/@player/HeaderCardPlayer.tsx
@@ -0,0 +1,16 @@
+import { PlayerStatus } from '../../@enums/appEnums';
+import Icon30x30Playlist from '../@icons/30x30/Icon30x30Playlist';
+
+export default function HeaderCardPlayer() {
+  const statusPlayer: PlayerStatus = PlayerStatus.NOW_PLAYING;
+  const openDialogPlaylist = () => {};
+
+  return (
+    <header className="relative flex h-16 items-center justify-center border p-2">
+      <button className="absolute left-0 text-perano-300" onClick={openDialogPlaylist}>
+        <Icon30x30Playlist />
+      </button>
+      <h3 className="text-center text-[1.35rem] font-semibold text-perano-300">{statusPlayer}</h3>
+    </header>
+  );
+}
