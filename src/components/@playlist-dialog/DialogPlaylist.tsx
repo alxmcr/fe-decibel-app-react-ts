0ca4aa93bb8dc8d5de389cb4a3643a6405205789
Alejandro Coca <alxmcr@outlook.com>
2024-04-27 01:06:18 -0400

    wip

--- src/components/@playlist-dialog/DialogPlaylist.tsx
+++ src/components/@playlist-dialog/DialogPlaylist.tsx
@@ -1,9 +1,14 @@
+import { MOCK_PLAYLIST_001 } from '../../@mocks/mock-playlists';
+import BoxPlaylistSongList from './BoxPlaylistSongList';
 import HeaderDialogPlaylist from './HeaderDialogPlaylist';
 
 export default function DialogPlaylist() {
+  const playlist = MOCK_PLAYLIST_001;
+
   return (
     <dialog>
       <HeaderDialogPlaylist />
+      <BoxPlaylistSongList songsInPlaylist={playlist.songs} />
     </dialog>
   );
 }
