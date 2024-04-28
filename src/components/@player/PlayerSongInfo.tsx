4ad57fa18e69f52ad1048a6d121c8dc9707fb358
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-04-28 00:21:15 -0400

    feature/select-song-on-playlist (#18)

--- src/components/@player/PlayerSongInfo.tsx
+++ src/components/@player/PlayerSongInfo.tsx
@@ -1,18 +1,23 @@
-import { flySong } from '../../@mocks/mock-songs';
+import React from 'react';
+import { PlaylistDataContext } from '../../providers/PlaylistProvider/PlaylistContext';
 
 export default function PlayerSongInfo() {
-  const currentSong = flySong;
+  const { currentSongPlaying } = React.useContext(PlaylistDataContext);
+
+  if (currentSongPlaying === null || currentSongPlaying === undefined) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col items-center gap-2">
       <img
-        src={currentSong.albumCoverUrl}
-        alt={currentSong.name}
+        src={currentSongPlaying.albumCoverUrl}
+        alt={currentSongPlaying.name}
         className="h-[250px] w-full rounded-lg shadow-lg md:h-[300px]"
       />
       <div>
-        <h1 className="text-center text-[1.5rem] font-semibold text-perano-600">{currentSong.name}</h1>
-        <h2 className="text-center text-gray-500">{currentSong.singer}</h2>
+        <h1 className="text-center text-[1.5rem] font-semibold text-perano-600">{currentSongPlaying.name}</h1>
+        <h2 className="text-center text-gray-500">{currentSongPlaying.singer}</h2>
       </div>
     </div>
   );
