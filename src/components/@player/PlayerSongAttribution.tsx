a0c5b735448fff4d645de82491650c7c2d23494f
Alejandro Coca <alxmcr@outlook.com>
2024-04-28 00:17:05 -0400

    wip

--- src/components/@player/PlayerSongAttribution.tsx
+++ src/components/@player/PlayerSongAttribution.tsx
@@ -1,13 +1,18 @@
-import { flySong } from '../../@mocks/mock-songs';
+import React from 'react';
+import { PlaylistDataContext } from '../../providers/PlaylistProvider/PlaylistContext';
 
 export default function PlayerSongAttribution() {
-  const currentSong = flySong;
+  const { currentSongPlaying } = React.useContext(PlaylistDataContext);
+
+  if (currentSongPlaying === null || currentSongPlaying === undefined) {
+    return null;
+  }
 
   return (
     <p className="text-center text-sm text-perano-300">
       <span>{`Music provided by: `}</span>
-      <a href={currentSong.attribution.linkDownload} className="underline underline-offset-4">
-        {currentSong.attribution.musicProvidedBy}
+      <a href={currentSongPlaying.attribution.linkDownload} className="underline underline-offset-4">
+        {currentSongPlaying.attribution.musicProvidedBy}
       </a>
     </p>
   );
--- src/components/@playlist-dialog/PlaylistSongListItem.tsx
+++ src/components/@playlist-dialog/PlaylistSongListItem.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { songPlaylist001 } from '../../@mocks/mock-songs-in-playlist';
 import { SongInPlaylistData } from '../../@types/serviceTypes';
+import {
+  PlaylistDataContext,
+  PlaylistDispatchContext,
+} from '../../providers/PlaylistProvider/PlaylistContext';
+import {
+  movePointerPositionAction,
+  selectSongToPlayAction,
+} from '../../store/@actions-creators/playlistActions';
 
 type Props = {
   songInPlaylist: SongInPlaylistData;
@@ -8,10 +15,13 @@ type Props = {
 
 export default function PlaylistSongListItem({ songInPlaylist }: Props) {
   const [isSelectedToPlay, setIsSelectedToPlay] = React.useState(false);
-  const currentSongPlaying = songPlaylist001;
+  const { currentSongPlaying } = React.useContext(PlaylistDataContext);
+  const dispatchPlaylist = React.useContext(PlaylistDispatchContext);
 
   const selectSongToPlay = () => {
-    console.log('Song selected', songInPlaylist);
+    selectSongToPlayAction(dispatchPlaylist, songInPlaylist);
+    // Move pointer
+    movePointerPositionAction(dispatchPlaylist, songInPlaylist.position);
   };
 
   React.useEffect(() => {
