79b18918e00f5411927fd45dc808a93c7884bae2
Alejandro Coca <alxmcr@outlook.com>
2024-04-29 14:22:36 -0400

    wip

--- src/components/@player/CardPlayer.tsx
+++ src/components/@player/CardPlayer.tsx
@@ -1,9 +1,29 @@
+import React from 'react';
+import { PlayerDataContext, PlayerDispatchContext } from '../../providers/PlayerProvider/PlayerContext';
+import { setIsPlayableAction } from '../../store/@actions-creators/playerActions';
 import FooterCardPlayer from './FooterCardPlayer';
 import HeaderCardPlayer from './HeaderCardPlayer';
 import PlayerSongAttribution from './PlayerSongAttribution';
 import PlayerSongInfo from './PlayerSongInfo';
 
 export default function CardPlayer() {
+  const player = React.useContext(PlayerDataContext);
+  const dispatchPlayer = React.useContext(PlayerDispatchContext);
+
+  // Loaded audio data
+  React.useEffect(() => {
+    const handleIsPlayable = () => {
+      setIsPlayableAction(dispatchPlayer, true);
+    };
+
+    player.audioToPlay?.addEventListener('loadeddata', handleIsPlayable);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      player.audioToPlay?.removeEventListener('loadeddata', handleIsPlayable);
+    };
+  }, [player, dispatchPlayer]);
+
   return (
     <article className="flex h-[668px] w-[320px] flex-col justify-between gap-4 rounded-lg border bg-white p-4 md:h-[700px] md:w-[360px]">
       <HeaderCardPlayer />
