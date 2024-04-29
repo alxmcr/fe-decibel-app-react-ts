6034a58ff4ff32f248e7f9f0c7f178720e704348
Alejandro Coca <alxmcr@outlook.com>
2024-04-29 14:39:01 -0400

    wip

--- src/components/@player/player-controls/PlayerControlsNextSong.tsx
+++ src/components/@player/player-controls/PlayerControlsNextSong.tsx
@@ -1,21 +1,8 @@
-import React from 'react';
-import {
-  PlaylistDataContext,
-  PlaylistDispatchContext,
-} from '../../../providers/PlaylistProvider/PlaylistContext';
-import { movePointerPositionAction } from '../../../store/@actions-creators/playlistActions';
 import Icon50x50NextSongFilled from '../../@icons/50x50/Icon50x50NextSongFilled';
 
 export default function PlayerControlsNextSong() {
-  const { currentPlaylist, pointerPositionSong } = React.useContext(PlaylistDataContext);
-  const dispatchPlaylist = React.useContext(PlaylistDispatchContext);
-
   const nextSong = () => {
-    const songsOnPlaylist = currentPlaylist.songs;
-
     console.log('next song');
-    console.log({ songsOnPlaylist, nroSongs: songsOnPlaylist.length, pointerPositionSong });
-    movePointerPositionAction(dispatchPlaylist, pointerPositionSong + 1);
   };
 
   return (
--- src/components/@player/player-controls/PlayerControlsPrevSong.tsx
+++ src/components/@player/player-controls/PlayerControlsPrevSong.tsx
@@ -1,21 +1,8 @@
-import React from 'react';
-import {
-  PlaylistDataContext,
-  PlaylistDispatchContext,
-} from '../../../providers/PlaylistProvider/PlaylistContext';
-import { movePointerPositionAction } from '../../../store/@actions-creators/playlistActions';
 import Icon50x50PrevSongFilled from '../../@icons/50x50/Icon50x50PrevSongFilled';
 
 export default function PlayerControlsPrevSong() {
-  const { currentPlaylist, pointerPositionSong } = React.useContext(PlaylistDataContext);
-  const dispatchPlaylist = React.useContext(PlaylistDispatchContext);
-
   const prevSong = () => {
-    const songsOnPlaylist = currentPlaylist.songs;
-
     console.log('prev song');
-    console.log({ songsOnPlaylist, nroSongs: songsOnPlaylist.length, pointerPositionSong });
-    movePointerPositionAction(dispatchPlaylist, pointerPositionSong - 1);
   };
 
   return (
