355b6cee9c1d9902356016d120a667a772d0d609
Alejandro Coca <alxmcr@outlook.com>
2024-04-30 02:16:20 -0400

    wip

--- src/components/@player/player-controls/PlayerControlsPlaySong.tsx
+++ src/components/@player/player-controls/PlayerControlsPlaySong.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { PlayerStatus } from '../../../@enums/appEnums';
 import { PlayerDataContext, PlayerDispatchContext } from '../../../providers/PlayerProvider/PlayerContext';
 import {
-  playAction,
-  updateElapsedTimeAction,
-  updateStatusPlayerAction,
+  playAction
 } from '../../../store/@actions-creators/playerActions';
 import Icon80x80Play from '../../@icons/80x80/Icon80x80Play';
 
@@ -20,35 +17,6 @@ export default function PlayerControlsPlaySong() {
     }
   };
 
-  // update elapsedTime
-  React.useEffect(() => {
-    const handleTimeUpdate = () => {
-      if (PlayerStatus.NOW_PLAYING === player.statusPlayer) {
-        if (player.isPlayableAudio) {
-          if (player.audioToPlay !== null && player.audioToPlay !== undefined) {
-            const { currentTime } = player.audioToPlay;
-            const elapsedTimeOnSeconds = currentTime !== undefined ? Math.floor(currentTime) : 0;
-
-            if (elapsedTimeOnSeconds <= player.durationOnSeconds) {
-              // Updated elapsed time
-              updateElapsedTimeAction(dispatchPlayer, elapsedTimeOnSeconds);
-              // End song
-              if (elapsedTimeOnSeconds === player.durationOnSeconds) {
-                updateStatusPlayerAction(dispatchPlayer, PlayerStatus.IDLE);
-              }
-            }
-          }
-        }
-      }
-    };
-
-    player.audioToPlay?.addEventListener('timeupdate', handleTimeUpdate);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      player.audioToPlay?.removeEventListener('timeupdate', handleTimeUpdate);
-    };
-  }, [player, dispatchPlayer]);
 
   return (
     <button onClick={playSong} className="text-perano-500">
