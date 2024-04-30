e902990f46bf2de123a964e95f9dd3f70c086024
Alejandro Coca <alxmcr@outlook.com>
2024-04-30 14:13:04 -0400

    wip

--- src/components/@playlist-dialog/DialogPlaylist.tsx
+++ src/components/@playlist-dialog/DialogPlaylist.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { DialogPlaylistRefContext } from '../../providers/DialogPlaylistProvider/DialogPlaylistContext';
-import { PlaylistDataContext } from '../../providers/PlaylistProvider/PlaylistContext';
-import BoxPlaylistSongList from '../@playlist-card/BoxPlaylistSongList';
-import HeaderDialogPlaylist from '../@playlist-card/HeaderDialogPlaylist';
+import CardPlaylist from '../@playlist-card/CardPlaylist';
 
 export default function DialogPlaylist() {
-  const { currentPlaylist } = React.useContext(PlaylistDataContext);
   const dialogRef = React.useContext(DialogPlaylistRefContext);
 
   return (
@@ -13,8 +10,7 @@ export default function DialogPlaylist() {
       className="flex h-[668px] w-[320px] flex-col justify-between gap-4 rounded-lg border bg-white md:h-[700px] md:w-[360px]"
       ref={dialogRef}
     >
-      <HeaderDialogPlaylist />
-      <BoxPlaylistSongList songsInPlaylist={currentPlaylist.songs} />
+      <CardPlaylist />
     </article>
   );
 }
