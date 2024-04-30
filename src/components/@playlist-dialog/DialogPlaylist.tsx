14a47634e22ea735214c600969d2f4016ef7a523
Alejandro Coca <alxmcr@outlook.com>
2024-04-30 14:43:27 -0400

    wip

--- src/components/@playlist-dialog/DialogPlaylist.tsx
+++ src/components/@playlist-dialog/DialogPlaylist.tsx
@@ -6,7 +6,10 @@ export default function DialogPlaylist() {
   const dialogRef = React.useContext(DialogPlaylistRefContext);
 
   return (
-    <dialog className="absolute rounded-lg bg-light-50 backdrop:bg-black/50" ref={dialogRef}>
+    <dialog
+      className="absolute h-[640px] w-[300px] rounded-lg bg-light-50 backdrop:bg-black/50"
+      ref={dialogRef}
+    >
       <CardPlaylist />
     </dialog>
   );
